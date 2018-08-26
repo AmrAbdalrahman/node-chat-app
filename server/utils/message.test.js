f7c0ce3a4407bbdd064883fb018d15714bcb0f07
AmrAlmagic <amr.abdalrahman94@gmail.com>
2018-08-26 21:35:08 +0200

    format timestamps using momentjs

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -27,7 +27,7 @@ describe('generateLocationMessage', () =>{
        var from = 'Deb';
        var latitude = 15;
        var longitude = 19;
-       var url = 'https://www.google.com/maps?q=15,19';
+       var url = 'https://www.google.com/maps/?q=15,19';
        var message = generateLocationMessage(from,latitude,longitude);
 
        expect(typeof message.createdAt).toBe('number');

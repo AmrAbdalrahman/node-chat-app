bd37d2713636c07b32f609bccfd3085af5f20d61
AmrAlmagic <amr.abdalrahman94@gmail.com>
2018-08-28 20:03:29 +0200

    Add users class and updateUserList event

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,8 +11,6 @@ function scrollToBottom (){
     var newMessageHeight = newMessage.innerHeight();
     var lastMessageHeight = newMessage.prev().innerHeight();
 
-
-
     if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
         messages.scrollTop(scrollHeight);
     }
@@ -20,7 +18,31 @@ function scrollToBottom (){
 }
 
 socket.on('connect',  function(){
-    console.log('Connected to server');
+    var params =jQuery.deparam(window.location.search);
+
+    socket.emit('join', params, function (err) {
+        if (err){
+            alert(err);
+            window.location.href = '/';
+        }else{
+            console.log('No error');
+
+        }
+    })
+
+
+});
+
+socket.on('updateUserList', function (users) {
+    var ol = jQuery('<ol></ol>');
+
+    users.forEach(function (user) {
+        ol.append(jQuery('<li></li>').text(user));
+    });
+
+
+ jQuery('#users').html(ol);
+
 
 });
 
